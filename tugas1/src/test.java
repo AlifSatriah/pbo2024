170add386da2f84a045612355b79391967a8ecab
AlifSatriah <azisahyonn@gmail.com>
2025-03-08 13:11:58 +0700

    Add files via upload

---
+++ tugas1/src/test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args) {
+        Database db = new Database();
+        db.open();
+        db.view();
+//        System.out.println(db.data);
+    }
+
+}
